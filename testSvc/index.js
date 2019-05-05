acc07dd07276e9bb4db4188ee87e749422095fef
Mic <mic@secureideas.com>
2019-05-05 15:47:36 -0400

    Added URL rewrite, formatted testSvc to standard

--- testSvc/index.js
+++ testSvc/index.js
@@ -1,14 +1,14 @@
-var restify = require('restify');
+var restify = require('restify')
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+function respond (req, res, next) {
+  res.send('hello ' + req.params.name)
+  next()
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+var server = restify.createServer()
+server.get('/hello/:name', respond)
+server.head('/hello/:name', respond)
 
-server.listen(3000, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(3000, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
