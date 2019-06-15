1c8512671bca11728f5f82669c0662b0ff0c6abf
Mic <mic@secureideas.com>
2019-06-15 13:53:00 -0400

    Stubbed in profile and feed services

---
+++ feedSvc/index.js
@@ -0,0 +1,11 @@
+import restify from 'restify'
+
+var server = restify.createServer({ name: 'authService' })
+
+server.use(restify.acceptParser(server.acceptable))
+server.use(restify.authorizationParser())
+server.use(restify.bodyParser())
+
+server.get('/', (res, req, next) => {
+  res.send('Hello feed service')
+})
