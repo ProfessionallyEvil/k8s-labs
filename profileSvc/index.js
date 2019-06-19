66c3439b8929d3be78bd7885b34df2ec54deedc5
Mic <mic@secureideas.com>
2019-06-18 22:19:21 -0400

    WIP checkin - working on hostname res issue on gateway

--- profileSvc/index.js
+++ profileSvc/index.js
@@ -6,6 +6,11 @@ server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.authorizationParser())
 server.use(restify.plugins.bodyParser())
 
-server.get('/', (res, req, next) => {
+server.use((req, res, next) => {
+  console.log('Request received: ', req)
+  return next()
+})
+
+server.get('/bar', (res, req, next) => {
   res.send('Hello profile service')
 })
