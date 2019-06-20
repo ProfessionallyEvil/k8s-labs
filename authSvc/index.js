7480a6c06f0b693fbceb7da221d6fea3d318617e
Mic <mic@secureideas.com>
2019-06-20 14:38:06 -0400

    Style fixes

--- authSvc/index.js
+++ authSvc/index.js
@@ -1,31 +1,31 @@
-const restify = require('restify');
-const OAuthServer = require('restify-oauth-server');
+const restify = require('restify')
+const OAuthServer = require('restify-oauth-server')
 
-var server = restify.createServer({name: 'authService'});
+var server = restify.createServer({ name: 'authService' })
 
 server.oauth = new OAuthServer({
-  model: {},
-});
+  model: {}
+})
 
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.authorizationParser());
-server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.authorizationParser())
+server.use(restify.plugins.bodyParser())
 
 // Node Oauth2 Server expects the token request to be x-www-url-formencoded according to the Oauth2 spec
 // Restify's body parser puts formencoded params in req.params, so we'll need a quick little bit of middleware to copy them over to the body
 server.use((req, res, next) => {
   if (req.headers['content-type'] === 'application/x-www-url-formencoded') {
-    req.body = req.params;
+    req.body = req.params
   }
-  return next();
-});
+  return next()
+})
 
-server.post('/token', server.oauth.token());
+server.post('/token', server.oauth.token())
 
 server.get('/secret', server.oauth.authenticate(), (req, res, next) => {
-  res.send('Authenticated!');
-});
+  res.send('Authenticated!')
+})
 
-server.listen(3000, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(3000, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
