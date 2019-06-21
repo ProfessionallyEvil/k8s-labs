0104e72c1093f653517f10badb2387ed7da5337f
Mic <mic@secureideas.com>
2019-06-20 23:18:45 -0400

    WIP checkin after troubleshooting express-gateway rewrite issue

--- feedSvc/index.js
+++ feedSvc/index.js
@@ -1,4 +1,12 @@
 const restify = require('restify')
+const mongoose = require('mongoose')
+
+const Post = mongoose.model('Post', new mongoose.Schema({
+  author: String,
+  message: String,
+  timestamp: Date,
+  replies: Array
+}))
 
 var server = restify.createServer({ name: 'feedService' })
 
@@ -6,10 +14,40 @@ server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.authorizationParser())
 server.use(restify.plugins.bodyParser())
 
-server.get('/', (res, req, next) => {
-  res.send('Hello feed service')
+server.use((req, res, next) => {
+  console.log('request received:', req.url)
+  return next()
 })
 
-server.listen(3000, function () {
+server.get('/by/:author/:page?', async (req, res, next) => {
+  console.log(`Get posts by ${req.params.author}`)
+  // let result = await Post.find({ author: req.params.author }).limit(10).skip(toPage(req.params.page) * 10).exec()
+  // console.log('query result', result)
+  // res.json(200, result)
+})
+
+mongoose.connect('mongodb://dbase:27017', { useNewUrlParser: true, dbName: 'arrrspace', user: 'root', pass: 'toor', auth: { authdb: 'admin' }, reconnectTires: 20, reconnectInterval: 1000 })
+
+server.listen(3000, async function () {
+  await seedData()
   console.log('%s listening at %s', server.name, server.url)
 })
+
+function toPage (pageParam) {
+  pageParam = Number(pageParam)
+  if (isNaN(pageParam) || pageParam < 0) {
+    pageParam = 1
+  }
+  return pageParam.toFixed(0)
+}
+
+async function seedData () {
+  await Post.deleteMany()
+  let post = new Post({
+    author: 'yondu',
+    message: `Hey y'all`,
+    timestamp: Date.now(),
+    replies: []
+  })
+  post.save()
+}
