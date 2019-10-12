3f58392b044cd9e8a336759cd3ca7c1e3d46ea24
corysabol <cory@secureideas.com>
2019-10-12 17:30:47 +0000

    implementing basic style and login form for Arrrspace appp; for now it's all a facade that doesn't really do anything

---
+++ app/src/components/Login.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<style></style>
+
+<login>
+  <input type='text'/>
+  <br/>
+  <input type='password'/>
+  <br/>
+  <button on:click|preventDefault="{e => console.log('clicked')}">Login</button>
+</login>
---
+++ app/src/routes/login.svelte
@@ -0,0 +1,11 @@
+<script>
+  import LoginForm from '../components/Login.svelte';
+</script>
+
+<style>
+</style>
+
+<h1>Arrr, What'r Yer Cyber Pirate Creds?</h1>
+
+<LoginForm>
+</LoginForm>
