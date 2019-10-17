9a03100959996f0d4c45f6bb1fa9bcbb667da860
Cory <cory@secureideas.com>
2019-10-17 06:28:01 +0000

    added rollup replace rule for process.env.API URL

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -21,7 +21,8 @@ export default {
 		plugins: [
 			replace({
 				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				'process.env.NODE_ENV': JSON.stringify(mode),
+				'process.env.APIURL': JSON.stringify(process.env.APIURL)
 			}),
 			svelte({
 				dev,
