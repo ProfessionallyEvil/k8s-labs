a15eb83b7e8bc6880b93d22ab13da25b850ce309
Cory <cory@secureideas.com>
2019-10-18 05:17:44 +0000

    added stupid hacky node http proxy to / of the gateway to proxy to internal services via the x-original-host and x-original-url headers

--- gateway/server.js
+++ gateway/server.js
@@ -1,6 +1,39 @@
 const path = require('path');
 const gateway = require('express-gateway');
+const http = require('http');
+const httpProxy = require('http-proxy')
+const url = require('url');
 
 gateway()
   .load(path.join(__dirname, 'config'))
   .run();
+
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer(function (req, res) {
+  // contruct the URL to proxy to
+  console.log(req.headers); 
+  if (!req.headers['x-original-host']) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('\nWelcome aboard the Arrrspace API!\n');
+    return;
+  }
+  const path = (req.headers['x-original-url'] ? req.headers['x-original-url'] : '/');
+  const targetUrl = url.parse(req.headers['x-original-host'] + path);
+  console.log(targetUrl);
+  console.log(targetUrl.href);
+  // otherwise proxy the request and response
+
+  let opts = {
+    target: (targetUrl.protocol === null ? 'http://' + targetUrl.href : targetUrl.href)
+  };
+
+  console.log(opts);
+  proxy.web(req, res, opts, function (e) {
+    console.log(e);
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.end(e.toString());
+  });
+});
+server.listen(9100);
