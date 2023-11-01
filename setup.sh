bb41c6c34435495dcef441f63876173163e37af1
Kirby Sabol <corysabol@users.noreply.github.com>
2023-10-31 23:40:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,8 @@ set -x
 IFS=$'\n\t'
 
 IMAGES=(
-  "app" 
   "gateway" 
-  "authSvc" 
   "jenkinsSvc" 
-  "feedSvc" 
-  "profileSvc"
-  "simpleStorage"
-  "dbaseSvc"
   "base"
 )
 DEPS=()
@@ -39,7 +33,7 @@ fi
 
 setup_cluster () {
   # todo allow for cli flag specifcation of the driver
-  minikube start --driver=docker --force
+  minikube start --driver=docker
   #minikube addons enable registry
 }
 
@@ -50,72 +44,15 @@ build_images () {
     echo -e "\n[+] ======== Building image $target ========\n"
     cat Dockerfile
     TARGET_LOWER=$(echo "$target" | tr '[:upper:]' '[:lower:]')
-    docker build -t arrrspace-$TARGET_LOWER:v1 .
+    minikube image build -t arrrspace-$TARGET_LOWER .
     cd ../
   done; 
-}
-
-push_images () {
-  for image in "${IMAGES[@]}"; do
-    IMAGE_LOWER=$(echo "$image" || tr '[:upper:]' '[:lower:]')
-    docker push $(minikube ip):5000/arrrspace-$IMAGE_LOWER:v1
-  done;
 }
 
 deploy () {
   kubectl apply -f k8s-resources/
 }
 
-if [[ "$#" -eq 0 ]]; then
-  exit 1
-fi;
-
-opt="$1"
-case $opt in
-  "createcluster")
-    echo -e "[+] Creating a local cluster...\n"
-    setup_cluster
-    echo "[!] Cluster created"
-    ;;
-  "buildimages")
-    echo "[!] Building Docker images"
-    build_images
-    ;;
-  "deployservices")
-    echo "[+] Applying K8S configs"
-    deploy
-    kubectl get deployments,services,pods
-    ;;
-  "all")
-    setup_cluster
-    build_images
-    deploy
-    ;;
-esac
-
-# Start up a cluster
-#if [[ "$1" -eq "createcluster" || "$1" -eq "all" ]]; then
-#  echo -e "[+] Creating a local cluster...\n"
-#  setup_cluster
-#  echo "[!] Cluster created"
-#fi;
-#
-#if [[ "$1" -eq "buildimages" || "$1" -eq "all" ]]; then
-#  echo "[!] Building Docker images"
-#  build_images
-#fi;
-#
-#if [[ "$1" -eq "pushimages" || "$1" -eq "all" ]]; then
-#  echo "[+] Loading docker images into k8s cluster"
-#  push_images
-#fi;
-#
-#if [[ "$1" -eq "deploy" || "$1" -eq "all" ]]; then
-#  echo "[+] Applying k8s configs"
-#  deploy
-#  kubectl get deployments,services,pods
-#fi;
-
-#MASTER_NODE_IP=$(sudo minikube ip)
-#echo "[!] cluster master node ip: ${MASTER_NODE_IP}"
-#echo "[!] All done :)"
+setup_cluster
+build_images
+deploy
