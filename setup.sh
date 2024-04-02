30e6fd02b09cd41f2aa410f6c079a2a1643b8d55
Cory Sabol <corysabol@users.noreply.github.com>
2024-04-02 06:59:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,10 +29,6 @@ nodes:
 EOF
 
   kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-  kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
 }
 
 build_images () {
